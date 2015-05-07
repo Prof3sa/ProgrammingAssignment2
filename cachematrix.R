81718b8f416706e47f4917f99463c6a71fc7a1ba
Prof3sa <atkins.jamin@gmail.com>
2015-05-06 21:31:22 -0400

    Edited function as required
    Working solution added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() converts a regular matrix objected into a cache-able matrix.
+## cacheSolve() makes the solve() routine cache-able for a particular object
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+ invs <- NULL
+        set <- function(y)
+        {
+                x <<- y
+                invs <<- NULL
+        }
+        
+        get <- function() x
+        
+        setInv <- function(inv) invs <- inv
+        getInv <- function() invs
+        list(set = set, get = get, setInv=setInv, getInv=getInv)  
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		inv = x$getInv()
+        if(!is.null(inv))
+        {
+                return(inv)
+        }
+        data <- x$get()
+        inv <-solve(data)
+        x$setInv(inv)
+        inv
 }
